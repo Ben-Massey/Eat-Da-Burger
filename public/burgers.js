044b40b68a7663a6fd7ff25eaffd52edc5ad4c2f
Ben-Massey <pojoben@gmail.com>
2018-11-22 00:15:26 -0600

    updated

---
+++ public/burgers.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".change-condition").on("click", function(event) {
+      var id = $(this).data("id");
+      var newCon = $(this).data("newcon");
+  
+      var newCondition = {
+        devoured: newCon
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newCondition
+      }).then(
+        function() {
+          console.log("changed condition to", newCon);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
