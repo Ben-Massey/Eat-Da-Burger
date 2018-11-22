0f97ea60078bc8591a8c359a0916cbd5000d0970
Ben-Massey <pojoben@gmail.com>
2018-11-21 22:39:19 -0600

    set up connectiona and orm

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,6 @@ use burgers_db;
 create table burgers (
     id int not null auto_increment,
     burger_name varchar(34) not null,
-    devoured true
+    devoured default false,
+    primary key (id)
 );
-
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ('bacon cheese burger');
+insert into burgers (burger_name) values ('sirloin surpreme');
+insert into burgers (burger_name) values ('crazy burger');
